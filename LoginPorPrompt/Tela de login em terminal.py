ea3cb60988a30cecf0c9a0fb4f1b8f95b4ca1c82
Rômulo Garcia Freitas <romulo.freitas.rg@gmail.com>
2022-02-27 19:05:20 -0300

    Primeiro commit

---
+++ LoginPorPrompt/Tela de login em terminal.py
@@ -0,0 +1,13 @@
+"""print('Bem-Vindo\n')
+
+while True:
+    user = input(str('Usuário\n'))
+    password = input('Senha')
+
+    break
+"""
+n = True
+while n:
+    des = input('escolha uma opção')
+    if des == 0:
+        n = False
