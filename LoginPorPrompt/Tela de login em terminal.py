5be0e52c21bc6677bb8501b6ab9d69bcf68708f0
Rômulo Garcia Freitas <romulo.freitas.rg@gmail.com>
2022-03-01 16:59:56 -0300

    Implementação de Registro de usuario

--- LoginPorPrompt/Tela de login em terminal.py
+++ LoginPorPrompt/Tela de login em terminal.py
@@ -1,11 +1,28 @@
 print("Bem-Vindo")
 def initialchoice():
     userchoice = True
+    decision = '0'
     while userchoice:
-        print("O que deseja fazer?: \n0 - Sair \n1 - Logar-se\n2 - Cadastrar-se\n")
-        descision = input(str('escolha uma opção:\n'))
-        if descision == '0':
+        print("O que deseja fazer?:")
+        print("0 - Sair \n1 - Logar-se\n2 - Cadastrar-se\n")
+        decision = input(str('escolha uma opção:\n'))
+        if decision == '0':   # Sair  
             break
+        elif decision == '1': # Login
+            pass    
+        elif decision == '2': # cadastro de usuarios
+            pass 
+        else:
+            print('Insira uma opção valida!!\n')
+
+
     
     return
+
+#def
+
+def register():
+
+
 initialchoice()
+
